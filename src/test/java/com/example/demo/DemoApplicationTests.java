709b0b94abfda0a70c0518e07eff152d85810341
unknown <jasserdr@gmail.com>
2022-08-08 07:21:10 +0000

    committed mnv validate install compiler,verify,test

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test number 1 successfully tested");
+	}
+	
+	@Test
+	void contextLoads1() {
+		System.out.println("test number 2 successfully tested");
+
+	}
+
+}
